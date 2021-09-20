afcf50c2b6e39a10a5e41575e419dba296522b59
danila <isaichkindanila@gmail.com>
2021-09-20 12:38:34 +0300

    fix Coturn files

--- run_coturn.sh
+++ run_coturn.sh
@@ -1,1 +1,10 @@
-/home/danila/Documents/work/abm/run_coturn.sh
+#!/bin/bash
+
+sudo docker rm -f coturn || true
+sudo docker run \
+    -v $(dirname $(readlink -f $0))/coturn.conf:/etc/coturn/turnserver.conf \
+    --network=host \
+    --rm -d \
+    --name coturn \
+    coturn/coturn:alpine
+sudo docker logs -f coturn
